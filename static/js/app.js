63606c4ff8ab2ae7486b176d0a8fb32b881d7ea9
Kelly Curtiss <klcurtiss@pthsd.net>
2020-09-22 14:28:51 -0400

    upate level 1 js

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,34 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+    console.log(tbody);
+
+//fill the table with data
+function fillTable(data){
+    tbody.html("");
+    data.forEach(dataRow => {
+         row =tbody.append("tr");
+
+        Object.entries(dataRow).forEach(function([key, value]){
+            cell = row.append("td").text(value)
+        });
+    });
+}
+fillTable(tableData)
+console.log(tableData)
+
+function filterSearch(){
+    d3.event.preventDefault()
+
+    date = d3.select("#datetime").property("value");
+    filterData = tableData;
+
+    if (date){
+        filterData = filterData.filter((row) => row.datetime === date);
+    }
+    fillTable(filterData);
+}
+
+d3.selectAll("#filter-btn").on("click", filterSearch);
+fillTable(tableData);
